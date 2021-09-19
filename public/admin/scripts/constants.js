55563c6810f0ef81ec827c3857b969a5902ede25
alazzuri <alexislazzuri@gmail.com>
2021-09-19 18:14:44 -0300

    Delete unused vars

--- public/admin/scripts/constants.js
+++ public/admin/scripts/constants.js
@@ -1,4 +1,3 @@
-// const BASE_URL = "http://localhost:5000/v1";
 const BASE_URL = "https://ctd-api-resto.herokuapp.com/v1";
 const CREATE_PRODUCT_BUTTON = document.querySelector("#createProduct");
 const CANCEL_SAVE_PRODUCT = document.querySelector("#cancelSaveProduct");
--- public/login/scripts/constants.js
+++ public/login/scripts/constants.js
@@ -1,4 +1,3 @@
-// const BASE_URL = "http://localhost:5000/v1";
 const BASE_URL = "https://ctd-api-resto.herokuapp.com/v1";
 const LOGIN_BUTTON = document.querySelector("#loginBtn");
 const CREATE_ACCOUNT_BUTTON = document.querySelector("#createAccountBtn");
