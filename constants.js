d41a20afa536dee0422496a017ab911847e9a42a
alazzuri <alexislazzuri@gmail.com>
2021-09-19 17:43:27 -0300

    styles fixing

--- constants.js
+++
@@ -1,9 +0,0 @@
-const BASE_URL = "https://ctd-api-resto.herokuapp.com";
-
-const API_ENDPOINTS = {
-  PRODUCTS: "/products",
-  USERS: "/users",
-  ORDERS: "orders",
-};
-
-module.exports = { BASE_URL, API_ENDPOINTS };
