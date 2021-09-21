12877bc139a8fe6c54e1529070d64be060e97743
alazzuri <alexislazzuri@gmail.com>
2021-09-21 18:03:44 -0300

    Fix port issue

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const userRoutes = require("./routes/user.routes");
 (function main() {
   const server = express();
   server.use(express.json(), CORS());
-  const port = 3000;
+  const port = process.env.PORT || 3000;
 
   // SET UP SERVER
   server.listen(port, () => {
