baad0409a052f82f6b29dd72a4066f4e7fda2f61
alazzuri <alexislazzuri@gmail.com>
2021-11-30 17:30:42 -0300

    Update endpoint

--- public/login/scripts/constants.js
+++ public/login/scripts/constants.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://ctd-api-resto.herokuapp.com/v1";
+const BASE_URL = "https://ctd-testing-resto-api.herokuapp.com//v1";
 const LOGIN_BUTTON = document.querySelector("#loginBtn");
 const CREATE_ACCOUNT_BUTTON = document.querySelector("#createAccountBtn");
 const LOADER_COMPONENT = document.querySelector("#loader");
