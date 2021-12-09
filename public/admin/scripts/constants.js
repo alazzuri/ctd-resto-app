6944206fe79c19414ec5fb22646904de8b11299f
alazzuri <alexislazzuri@gmail.com>
2021-12-09 12:06:45 -0300

    Fix endpoint

--- public/admin/scripts/constants.js
+++ public/admin/scripts/constants.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://ctd-testing-resto-api.herokuapp.com//v1";
+const BASE_URL = "https://ctd-testing-resto-api.herokuapp.com/v1";
 const CREATE_PRODUCT_BUTTON = document.querySelector("#createProduct");
 const CANCEL_SAVE_PRODUCT = document.querySelector("#cancelSaveProduct");
 const SAVE_PRODUCT = document.querySelector("#saveProduct");
--- public/register/scripts/constants.js
+++ public/register/scripts/constants.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://ctd-testing-resto-api.herokuapp.com//v1";
+const BASE_URL = "https://ctd-testing-resto-api.herokuapp.com/v1";
 const REGISTER_BUTTON = document.querySelector("#registerButton");
 const SIGNIN_BUTTON = document.querySelector("#signinButton");
 const LOADER_COMPONENT = document.querySelector("#loader");
