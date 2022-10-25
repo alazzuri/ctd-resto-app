d546d8747747a63ed9e72d2e83205a6c93993ffa
Alexis <alexis.lazzuri@intive-fdv.com>
2022-10-25 18:03:58 -0300

    Change API url

--- public/user/scripts/constants.js
+++ public/user/scripts/constants.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://ctd-testing-resto-api.herokuapp.com/v1";
+const BASE_URL = "https://ctd-api-resto.herokuapp.com/v1";
 const CONFIRM_ORDER_BUTTON = document.querySelector("#confirmOrder");
 const LOGOUT_BUTTON = document.querySelector("#logout");
 const LOADER_COMPONENT = document.querySelector("#loader");
